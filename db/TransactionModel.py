e7a8f15d2a7142415bb5d09f4f29eb71bcd0fea9
gktatla <kirantatla5@gmail.com>
2020-09-25 19:55:58 -0400

    did buy order

--- db/TransactionModel.py
+++ db/TransactionModel.py
@@ -5,8 +5,8 @@ from app import db
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     stock_symbol = db.Column(db.String(4), nullable=False)
-    buy_order_id = db.Column(db.Integer, db.ForeignKey('buy_orders.id'), nullable=False)
-    sell_order_id = db.Column(db.Integer, db.ForeignKey('sell_orders.id'), nullable=False)
+    buy_order_id = db.Column(db.Integer, db.ForeignKey('buy_order.id'), nullable=False)
+    sell_order_id = db.Column(db.Integer, db.ForeignKey('sell_order.id'), nullable=False)
     units = db.Column(db.Integer, nullable=False)
     price = db.Column(db.DECIMAL(65,2), nullable=False)
 
--- routes/order.py
+++ routes/order.py
@@ -1,16 +1,37 @@
 from flask_restful import Api, Resource
-
+import json
 from flask import request
 
 from db.BuyOrderModel import BuyOrder, buy_order_schema
-# from db.SellOrderModel import SellOrder, sell_order_schema
-# from db.TransactionModel import Transaction, transaction_schema
+from db.SellOrderModel import SellOrder, sell_order_schema
+from db.TransactionModel import Transaction, transaction_schema
 
+from app import db
 
 class OrderResource(Resource):
-    def post(self):
-    	print(request.json)
-
+    def post(self):    	
+    	if request.json["type"] == "buy":
+    		new_order = BuyOrder(
+    			user_id = request.json["type"],
+    			stock_symbol = request.json["stock_symbol"],
+    			units = request.json["units"],
+    			units_fulfilled = 0,
+    			limit_price = request.json["limit_price"]
+    		)
+    		db.session.add(new_order)
+    		db.session.commit()
+    		return { "statusCode": 200 }
+    	else:
+    		new_order = SellOrder(
+    			user_id = request.json["type"],
+    			stock_symbol = request.json["stock_symbol"],
+    			units = request.json["units"],
+    			units_fulfilled = 0,
+    			limit_price = request.json["limit_price"]
+    		)
+	    	db.session.add(new_order)
+	    	db.session.commit()
+	    	return { "statusCode": 200 }
 
     def patch(self):
         print ("patch!")

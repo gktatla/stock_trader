cd09b242eb0174c816d5e39ab6c6a01def04875a
gktatla <kirantatla5@gmail.com>
2020-10-01 18:19:46 -0400

    added unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -12,33 +12,52 @@ from db.TransactionModel import Transaction, transaction_schema
 from db.BuyOrderModel import BuyOrder, buy_order_schema
 from db.SellOrderModel import SellOrder, sell_order_schema
 
+
+
 class TestApp(unittest.TestCase):
 
-    test_order = BuyOrder(
-            user_id = 1,
+    test_buy_order = BuyOrder(
+            user_id = 9,
             stock_symbol = "zm",
             units = 20,
             units_fulfilled = 0,
             limit_price = 20.00
     )
 
-    test_sell_list = [SellOrder(id = 1, user_id = 1, stock_symbol = "zm", units = "20", units_fulfilled = "0", limit_price = "50.00", order_time = datetime.now()),
-    SellOrder(id = 2, user_id = 2, stock_symbol = "zm", units = "20", units_fulfilled = "0", limit_price = "30.00", order_time = datetime.now()),
-    SellOrder(id = 3, user_id = 3, stock_symbol = "zm", units = "20", units_fulfilled = "0", limit_price = "60.00", order_time = datetime.now()),
-    SellOrder(id = 3, user_id = 3, stock_symbol = "zm", units = "20", units_fulfilled = "0", limit_price = "30.00", order_time = datetime.now()-timedelta(days=1)),
-    SellOrder(id = 4, user_id = 4, stock_symbol = "zm", units = "20", units_fulfilled = "0", limit_price = "80.00", order_time = datetime.now())]
+    no_match_sell_order = SellOrder(
+            user_id = 9,
+            stock_symbol = "zm",
+            units = 20,
+            units_fulfilled = 0,
+            limit_price = 200.00
+    )
 
-    test_buy_list = [BuyOrder(id = 1, user_id = 5, stock_symbol = "zm", units = "20", units_fulfilled = "0", limit_price = "50.00", order_time = datetime.now()),
-    BuyOrder(id = 2, user_id = 6, stock_symbol = "zm", units = "20", units_fulfilled = "0", limit_price = "30.00", order_time = datetime.now()),
-    BuyOrder(id = 3, user_id = 7, stock_symbol = "zm", units = "20", units_fulfilled = "0", limit_price = "60.00", order_time = datetime.now()),
-    BuyOrder(id = 4, user_id = 8, stock_symbol = "zm", units = "20", units_fulfilled = "0", limit_price = "50.00", order_time = datetime.now())]
+    no_match_buy_order = BuyOrder(
+            user_id = 9,
+            stock_symbol = "zm",
+            units = 20,
+            units_fulfilled = 0,
+            limit_price = 5.00
+    )
+
+    test_sell_list = [SellOrder(id = 1, user_id = 1, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 50.00, order_time = datetime.now()),
+    SellOrder(id = 2, user_id = 2, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 30.00, order_time = datetime.now()),
+    SellOrder(id = 3, user_id = 3, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 60.00, order_time = datetime.now()),
+    SellOrder(id = 3, user_id = 3, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 30.00, order_time = datetime.now()),
+    SellOrder(id = 4, user_id = 4, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 80.00, order_time = datetime.now())]
+
+    test_buy_list = [BuyOrder(id = 1, user_id = 5, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 100.00, order_time = datetime.now()),
+    BuyOrder(id = 2, user_id = 6, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 90.00, order_time = datetime.now()),
+    BuyOrder(id = 3, user_id = 7, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 110.00, order_time = datetime.now()),
+    BuyOrder(id = 4, user_id = 8, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 85.00, order_time = datetime.now()),
+    BuyOrder(id = 5, user_id = 9, stock_symbol = "zm", units = 20, units_fulfilled = 0, limit_price = 20.00, order_time = datetime.now())]
 
     @mock.patch('app.db.session')
     def test_matching_no_sell_list(self, mock_session): 
     	#first is sell, second is buy
         mock_session.query.return_value.filter.return_value.filter.return_value.with_for_update.return_value.order_by.return_value.order_by.return_value.all.side_effect = [None, self.test_buy_list]
 
-        results = matching(self.test_order, "buy")
+        results = matching(self.test_buy_order, "buy")
         self.assertEqual(results, "no sell list")
 
     @mock.patch('app.db.session')
@@ -46,16 +65,23 @@ class TestApp(unittest.TestCase):
     	#first is sell, second is buy
     	mock_session.query.return_value.filter.return_value.filter.return_value.with_for_update.return_value.order_by.return_value.order_by.return_value.all.side_effect = [self.test_sell_list, None]
 
-    	results = matching(self.test_order, "buy")
+    	results = matching(self.test_buy_order, "buy")
     	self.assertEqual(results, "no buy list")
 
-    # @mock.patch('app.db.session')
-    # def test_no_matching_buy(self, mock_session):
-    # 	#first is sell, second is buy 
-    #     mock_session.query.return_value.filter.return_value.filter.return_value.with_for_update.return_value.order_by.return_value.order_by.return_value.all.side_effect = [self.test_sell_list, self.test_buy_list]
+    @mock.patch('app.db.session')
+    def test_no_matching_buy(self, mock_session):
+    	#first is sell, second is buy 
+        mock_session.query.return_value.filter.return_value.filter.return_value.with_for_update.return_value.order_by.return_value.order_by.return_value.all.side_effect = [self.test_sell_list, self.test_buy_list]
 
-    #     #sell order to update, buy order to update
-    #     mock_session.query.return_value.filter.return_value.with_for_update.return_value.one.return_value = [self.test_sell_list[3], self.test_order]
+        #insert sell order that won't have a match
+        results = matching(self.no_match_sell_order, "sell")
+        self.assertEqual(results, "order has no match")
 
-    #     results = matching(self.test_order, "buy")
-    #     self.assertEqual(results, "order has no match")
+    @mock.patch('app.db.session')
+    def test_no_matching_sell(self, mock_session):
+    	#first is sell, second is buy 
+        mock_session.query.return_value.filter.return_value.filter.return_value.with_for_update.return_value.order_by.return_value.order_by.return_value.all.side_effect = [self.test_sell_list, self.test_buy_list]
+
+        #insert buy order that will have no match
+        results = matching(self.no_match_buy_order, "buy")
+        self.assertEqual(results, "order has no match")
